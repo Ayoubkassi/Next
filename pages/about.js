6effa64fbe85bd87dc04fbc213fb013dc639e683
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-08-29 21:59:15 +0100

    add page

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+const About = () => {
+  return (
+    <div>
+    <Head>
+      <meta name='keywords' content='ayoubkassi ,ayoub,kassi' />
+      <title>About< /title>
+    </Head>
+      <h1 style={{color : 'red'}}>About</h1>
+    </div>
+  );
+}
+
+export default About;
