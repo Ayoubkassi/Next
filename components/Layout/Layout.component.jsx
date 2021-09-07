f809de90c287d34d29c8ad77b964e38aff387e73
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-09-07 22:50:40 +0100

    add bootsrap react

--- components/Layout/Layout.component.jsx
+++ components/Layout/Layout.component.jsx
@@ -1,9 +1,10 @@
 import styles from '../../styles/Layout.module.css';
-
+import { Navbar } from '../Navbar/Navbar.component';
 
 const Layout = ({children}) => {
   return(
     <div className={styles.container}>
+      <Navbar />
       <main className={styles.main}>
         { children }
       </main>
